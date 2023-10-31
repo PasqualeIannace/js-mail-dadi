d8bf44ddb768b96e0985d08fb43d01a12c20e292
PasqualeIannace <pasquale.iannace@gmail.com>
2023-10-31 17:08:12 +0100

    Aggiunto gioco dadi

--- js/main.js
+++ js/main.js
@@ -21,3 +21,32 @@ btn.addEventListener("click", function() {
         }
     }
 });
+
+// GIOCHIAMO CON I DADI
+
+const btnPlay = document.getElementById("playBtn");
+
+let playerCounter = 0;
+let pcCounter = 0;
+
+
+btnPlay.addEventListener("click", function() {
+
+    const dadoUmano = Math.floor(Math.random() * 6) + 1;
+    const dadoComputer = Math.floor(Math.random() * 6) + 1;
+    console.log("Player: ", dadoUmano);
+    console.log("PC: " ,dadoComputer);
+
+
+    if (dadoUmano > dadoComputer) {
+        playerCounter++;
+    } else if (dadoUmano < dadoComputer) {
+        pcCounter++;
+    } else {
+        console.log("Pareggio!")
+    }
+
+    console.log("Vittorie Player: ", playerCounter);
+    console.log("Vittorie PC: ", pcCounter);
+
+})
